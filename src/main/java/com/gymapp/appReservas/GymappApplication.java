e257bd4b9656644b3fd0c80c072a3c86a2ca9b43
MIKE224 <miguel@siemprendedor.com>
2019-11-29 13:55:46 +0100

    v1

--- src/main/java/com/gymapp/appReservas/GymappApplication.java
+++ src/main/java/com/gymapp/appReservas/GymappApplication.java
@@ -1,4 +1,4 @@
-package com.gymapp.gymapp;
+package com.gymapp.appReservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/gymapp/appReservas/entity/UsuarioSimplificado.java
@@ -0,0 +1,6 @@
+package com.gymapp.appReservas.entity;
+
+public interface UsuarioSimplificado {
+    public String getNombre();
+    public String getApellidos();
+}
